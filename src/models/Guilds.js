670ff03cb611232cc408c825bb4434637f86bf8a
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2021-12-07 12:18:10 +0600

    Add files via upload

---
+++ src/models/Guilds.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose');
+
+module.exports = model('Guilds', new Schema({
+    guildID: { type: String },
+    prefix: { type: String, default: '!' },
+    language: { type: String, default: 'en' },
+    welcomeChannel: { type: String, default: null },
+}))
